957272f0967bec2413aa4e5a23a545610920fe74
Johannes Nicolaus Wibisana <johannes.nicolaus@gmail.com>
2020-06-11 16:05:23 +0900

    Add scripts

---
+++ scripts/GTF_to_translation_start.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# extract bed files containing coordinates of TSS translation start site of  genome
+# example usage GTF_to_TSS.sh x.GTF y.bed
+
+# g is global (replaces all of semicolon)
+cat $1 | awk '$3 == "CDS"' | awk '$7 == "+"' | awk '{print $1, $5-1000, $5+1000, $10, $14}' | sed 's/ /\t/g' | sed 's/;//g' > gtf_forward.bed 
+
+cat $1 | awk '$3 == "CDS"' | awk '$7 == "-"' | awk '{print $1, $5-1000, $5+1000, $10, $14}' | sed 's/ /\t/g' | sed 's/;//g' > gtf_reverse.bed
+
+printf "chr\tstart\tend\tensembl_gene_id\tensembl_transcript_id\n" > header.bed
+
+cat ./header.bed ./gtf_reverse.bed ./gtf_forward.bed > $2
+
+# remove files
+rm gtf_reverse.bed 
+rm gtf_forward.bed
+rm header.bed
